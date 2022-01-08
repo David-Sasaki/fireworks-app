f9079ccea95df91dc55bff950399c37759f8757b
David-Sasaki <davidsasaki1986@gmail.com>
2022-01-07 21:46:01 -0500

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ interface Firework {
   y: number;
   color: string;
   size: number;
-  explosionRadius: number;
   explosionDuration: number;
   exploded: boolean;
   targetX: number;
@@ -17,20 +16,26 @@ interface Firework {
 interface Particle {
   x: number;
   y: number;
-  size: number;
   color: string;
+  size: number;
   speed: number;
+  angle: number;
   alpha: number;
+  gravity: number;
 }
 
 const App: React.FC = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  // State variables
   const [fireworks, setFireworks] = useState<Firework[]>([]);
   const [explosionRadius, setExplosionRadius] = useState<number>(30);
   const [explosionDuration, setExplosionDuration] = useState<number>(3);
   const [fireworkColor, setFireworkColor] = useState<string>("#ffffff");
   const [fireworkSize, setFireworkSize] = useState<number>(3);
 
+  // Canvas reference
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  // Animation loop effect
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
@@ -39,8 +44,10 @@ const App: React.FC = () => {
     if (!ctx) return;
 
     const interval = setInterval(() => {
+      // Clear canvas
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+      // Update and draw fireworks
       fireworks.forEach((firework, index) => {
         if (!firework.exploded) {
           drawFirework(ctx, firework);
@@ -55,9 +62,11 @@ const App: React.FC = () => {
       });
     }, 50);
 
+    // Clean up interval on unmount
     return () => clearInterval(interval);
   }, [fireworks]);
 
+  // Drawing functions
   const drawFirework = (ctx: CanvasRenderingContext2D, firework: Firework) => {
     ctx.beginPath();
     ctx.arc(firework.x, firework.y, firework.size, 0, Math.PI * 2);
@@ -66,33 +75,29 @@ const App: React.FC = () => {
   };
 
   const drawExplosion = (ctx: CanvasRenderingContext2D, firework: Firework) => {
-    for (let i = 0; i < 100; i++) {
-      const angle = Math.random() * Math.PI * 2;
-      const speed = Math.random() * 3 + 1; // Random speed between 1 and 4
-      const radius = Math.random() * firework.explosionRadius;
+    const explosionParticles = 30;
 
-      // Calculate particle position
-      const x = firework.x + Math.cos(angle) * radius;
-      const y = firework.y + Math.sin(angle) * radius;
+    if (firework.particles.length === 0) {
+      for (let i = 0; i < explosionParticles; i++) {
+        const angle = Math.random() * Math.PI * 2;
+        const speed = Math.random() * 3 + 1;
+        const gravity = Math.random() * 2 + 1;
 
-      // Random size and color for each particle
-      const size = Math.random() * 3 + 1; // Random size between 1 and 4
-      const color = `hsl(${Math.random() * 360}, 100%, 50%)`; // Random color
+        const newParticle: Particle = {
+          x: firework.x,
+          y: firework.y,
+          color: firework.color,
+          size: Math.random() * 3 + 1,
+          speed: speed,
+          angle: angle,
+          alpha: 1,
+          gravity: gravity,
+        };
 
-      const newParticle: Particle = {
-        x: x,
-        y: y,
-        size: size,
-        color: color,
-        speed: speed,
-        alpha: 1,
-      };
-
-      // Draw the particle
-      drawParticle(ctx, newParticle);
-
-      // Store particle properties for animation
-      firework.particles.push(newParticle);
+        firework.particles.push(newParticle);
+      }
+    } else {
+      firework.particles.forEach((particle) => drawParticle(ctx, particle));
     }
   };
 
@@ -105,6 +110,7 @@ const App: React.FC = () => {
     ctx.globalAlpha = 1; // Reset global alpha
   };
 
+  // Movement functions
   const moveFirework = (firework: Firework, index: number) => {
     const dx = firework.targetX - firework.x;
     const dy = firework.targetY - firework.y;
@@ -122,37 +128,46 @@ const App: React.FC = () => {
       firework.y += velocityY;
     }
 
-    fireworks[index] = firework;
-    setFireworks([...fireworks]);
+    setFireworks([
+      ...fireworks.slice(0, index),
+      firework,
+      ...fireworks.slice(index + 1),
+    ]);
   };
 
   const moveParticles = (firework: Firework, index: number) => {
-    const gravity = 10,
-      angle = 45;
     for (let i = 0; i < firework.particles.length; i++) {
       const particle = firework.particles[i];
 
-      particle.x += Math.cos(angle) * particle.speed;
-      particle.y += Math.sin(angle) * particle.speed;
+      particle.x += Math.cos(particle.angle) * particle.speed;
+      particle.y += Math.sin(particle.angle) * particle.speed;
 
-      particle.y += gravity;
+      // Apply gravity
+      particle.y += particle.gravity;
 
-      particle.alpha -= 0.01;
+      // Reduce alpha (transparency) gradually
+      particle.alpha -= 0.02;
 
+      // If the particle's alpha becomes less than or equal to 0, remove it from the array
       if (particle.alpha <= 0) {
         firework.particles.splice(i, 1);
-        i--;
+        i--; // Adjust index due to removal of element
       }
     }
 
+    // If all particles have faded away, mark the firework as exploded
     if (firework.particles.length === 0) {
       firework.exploded = true;
     }
 
-    fireworks[index] = firework;
-    setFireworks([...fireworks]);
+    setFireworks([
+      ...fireworks.slice(0, index),
+      firework,
+      ...fireworks.slice(index + 1),
+    ]);
   };
 
+  // Event handler for canvas click
   const handleClick = (
     event: React.MouseEvent<HTMLCanvasElement, MouseEvent>
   ) => {
@@ -168,7 +183,6 @@ const App: React.FC = () => {
       y: canvas.height,
       color: fireworkColor,
       size: fireworkSize,
-      explosionRadius: explosionRadius,
       explosionDuration: explosionDuration,
       exploded: false,
       targetX: x,
@@ -179,6 +193,7 @@ const App: React.FC = () => {
     setFireworks([...fireworks, newFirework]);
   };
 
+  // Render
   return (
     <div className="App">
       <canvas
@@ -226,3 +241,4 @@ const App: React.FC = () => {
 };
 
 export default App;
+
